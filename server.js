fdfc13dfe3446ad6b67cf056a65e4fcc0a244bce
huythe-nguyen <nguyenhuythe1997@gmail.com>
2022-01-04 13:51:36 +0700

    up

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/admin-panel-layout'));
+app.use(express.static('./dist/18110220_18110220_18110128_TLCN_FEAdmin'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/admin-panel-layout/'}),
+    res.sendFile('index.html', {root: 'dist/18110220_18110220_18110128_TLCN_FEAdmin/'}),
 );
 
 // Start the app by listening on the default Heroku port
